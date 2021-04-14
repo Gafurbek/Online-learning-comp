38ac99b4cad9fa113735cdf3df8e73917266e77b
Gafurbek <gafurbek>
2021-04-15 00:39:38 +0500

    versia 1.0.0

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const menuBtn = document.querySelector('.menu__btn'),
+    closeBtn = document.querySelector('.close__btn'),
+    menu = document.querySelector('.nav__link');
+
+
+menuBtn.addEventListener('click', event => menu.style.transform = 'translateY(0)');
+
+closeBtn.addEventListener('click', event => menu.style = clientInformation)
